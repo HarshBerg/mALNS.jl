106e7b87f5c8192a83da4eb0d205ce0184910bc9
HarshBerg <harshuket@gmail.com>
2025-09-01 20:05:36 +0530

    Solution created

--- src/DataStructure.jl
+++ src/DataStructure.jl
@@ -1,13 +1,18 @@
-# Arc is line path joining two points
-# Tailnode and headnode are the two points
-
+""" 
+    Arc(iᵗ::Int, iʰ::Int, l::Float64)
+    An 'Arc' is a directed edge from node iᵗ (tail) to node iʰ (head) with length l.
+"""
 struct Arc
     iᵗ::Int                   # Index of the tail node
     iʰ::Int                   # Index of the head node
     l::Float64                 # Length of the arc                
 end
 
-# Route is one loop from depot to depot; includes vehicle index, coordinates of the route, total customers served, length total
+
+"""
+    Route(iᵛ::Int, x::Vector{Float64}, y::Vector{Float64}, n::Int, qᶜ::Float64, l::Float64)
+    A 'Route' represents a loop from depot to depot served by vehicle iᵛ, with coordinates (x,y), number of customers n, total demand qᶜ, and length l.
+"""
 mutable struct Route
     iᵛ::Int                   # Vehicle index
     x::Vector{Float64}        # x-Coordinates of the route
@@ -17,6 +22,11 @@ mutable struct Route
     l::Float64                # Length of the route
 end
 
+
+"""
+    Vehicle(iᵛ::Int, qᵛ::Float64, π::Float64, R::Vector{Route})
+    A 'Vehicle' has index iᵛ, capacity qᵛ, operating cost π per unit distance, and a vector of routes R.
+"""
 # Vehicle has index, capacity, operating cost, and a vector of routes
 mutable struct Vehicle
     iᵛ::Int                   # Vehicle index
@@ -25,17 +35,29 @@ mutable struct Vehicle
     R::Vector{Route}          # Vector of routes
 end
 
-# node is a point in the graph
+
+"""
+    Node
+    An abstract type 'Node' representing a node in the network.
+"""
 abstract type Node end
 
-# Depot is a node with coordinates
+
+"""
+    Depot(x::Float64, y::Float64, V::Vector{Vehicle})
+    A 'Depot' is a node with coordinates (x,y) and a vector of vehicles V.
+"""
 struct Depot <: Node
     x::Float64                # x-Coordinate of the depot
     y::Float64                # y-Coordinate of the depot
     V::Vector{Vehicle}        # Vector of vehicles at the depot
 end
 
-# Customer is a node with coordinates, index and demand
+
+"""
+    Customer(iⁿ::Int, x::Float64, y::Float64, qᶜ::Float64, iᵗ::Int, iʰ::Int, iʳ::Int, iᵛ::Int)
+    A 'Customer' has index iⁿ, coordinates (x,y), demand qᶜ, indices of tail and head depots (iᵗ, iʰ), route index iʳ, and vehicle index iᵛ.
+"""
 mutable struct Customer <: Node
     iⁿ::Int                   # Customer index
     x::Float64                # x-Coordinate of the customer
@@ -47,9 +69,16 @@ mutable struct Customer <: Node
     iᵛ::Int                   # Vehicle index serving this customer
 end
 
-# solution is a collection of routes and vehicles
+
+"""
+    Solution(C::Vector{Customer}, D::Vector{Depot}, A::Vector{Arc})
+    A 'Solution' consists of vectors of customers C, depots D, and arcs A.
+"""
 mutable struct Solution
     C::Vector{Customer}       # Vector of customers
+    D::Vector{Depot}          # Vector of depots
+    A::Vector{Arc}            # Vector of arcs 
+end
 
   
 
