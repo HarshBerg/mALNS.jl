59a132616f088af50adb0c8f782945b786e1cc5b
HarshBerg <harshuket@gmail.com>
2025-09-07 15:18:39 +0530

    changed node and definations

--- src/datastructure.jl
+++ src/datastructure.jl
@@ -1,22 +1,25 @@
 
-# TODO: Add definition
+"""
+    Node(i::Int, x::Float64, y::Float64, q::Float64, t::Int, h::Int, v::Int)
+    A 'Node' has index i, coordinates (x, y), demand q, tail node index t, head node index h,
+    and vehicle index v serving this customer.
+"""
 mutable struct Node
-    i::Int                   # Customer index
-    x::Float64                # x-Coordinate of the customer
-    y::Float64                # y-Coordinate of the customer
-    q::Float64               # Customer demand
+    i::Int                   # node index
+    x::Float64               # x-Coordinate of the node
+    y::Float64               # y-Coordinate of the node
+    q::Float64               # node demand
     t::Int                   # Index of the tail node (depot)
     h::Int                   # Index of the head node (depot)
     v::Int                   # Vehicle index serving this customer
     # TODO: Define the new customer
-    Node(# some inputs) = new(# all input values)
+    #Node (some inputs) = new(# all input values)
 end
 
 """ 
-    Arc(iᵗ::Int, iʰ::Int, l::Float64)
-    An 'Arc' is a directed edge from node iᵗ (tail) to node iʰ (head) with length l.
+    Arc(i::Int, j::Int, l::Float64)
+    An 'Arc' is a directed edge from node i (tail) to node j (head) with length l.
 """
-# TODO: Update the definition
 struct Arc
     i::Int                   # Index of the tail node
     j::Int                   # Index of the head node
@@ -24,26 +27,24 @@ struct Arc
 end
 
 """
-    Vehicle(iᵛ::Int, qᵛ::Float64, π::Float64, R::Vector{Route})
-    A 'Vehicle' has index iᵛ, capacity qᵛ, operating cost π per unit distance, and a vector of routes R.
+    Vehicle(i::Int, s::Int, e::Int, q::Float64, n::Int, l::Float64, x::Vector{Float64}, y::Vector{Float64})
+    A 'Vehicle' has index i, starts at node s, ends at node e, has capacity q, serves n customers,
+    total demand l, and follows a route defined by coordinates x and y.
 """
-# TODO: Update the definition
 mutable struct Vehicle
-    i::Int                   # Vehicle index
-    # TODO: Add start and end node Index
-    q::Float64               # Vehicle capacity
+    i::Int                    # Vehicle index
+    s::Int                    # Start node index
+    e::Int                    # End node index
+    q::Float64                # Vehicle capacity
     n::Int                    # Number of customers served
     l::Float64                # Total demand served by the route
-    x::Vector{Float64}        # x-Coordinates of the route
-    y::Vector{Float64}        # y-Coordinates of the route
-    # TODO: Define the new vehicle
+    
 end
 
 """
-    Solution(C::Vector{Customer}, D::Vector{Depot}, A::Vector{Arc}, c::Float64)
-    A 'Solution' consists of vectors of customers C, depots D, and arcs A with cost c.
+    Solution(N::Vector{Node}, A::Matrix{Arc}, c::Float64)
+    A 'Solution' consists of a vector of nodes N, a matrix of arcs A, and a total cost c.
 """
-# TODO: Update Definition
 mutable struct Solution
     N::Vector{Node}           # Vector of Nodes
     A::Matrix{Arc}            # Matrix of arcs 
