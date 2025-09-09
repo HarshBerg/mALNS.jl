48134c46b301f0fab70e39e52e7937c98f7a240f
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-09-09 11:48:49 +0530

    update build

--- src/initialize.jl
+++ src/initialize.jl
@@ -7,9 +7,9 @@ function build(instance::String; dir=joinpath(dirname(@__DIR__), "instances"))
     k₃ = findfirst(contains("NODE_COORD_SECTION"), df[:,1])
     k₄ = findfirst(contains("DEMAND_SECTION"), df[:,1])
     # nodes
-    # NOTE: made some improvements (remove this comment once you are satisfied with the changes)
     n = parse(Int, df[k₁,2])
     N = Vector{Node}(undef, n)
+    # TODO: check outcomes for x, y, and q
     for i in 1:n
         x = parse(Int, df[k₃+i, 2])
         y = parse(Int, df[k₃+i, 3])
@@ -17,7 +17,6 @@ function build(instance::String; dir=joinpath(dirname(@__DIR__), "instances"))
         N[i] = Node(i, x, y, q, 0, 0, 0)
     end
     # arcs
-    # NOTE: made some improvements (remove this comment once you are satisfied with the changes)
     A = Matrix{Arc}(undef, n, n)
     for i in 1:n
         xᵢ = N[i].x
@@ -25,17 +24,16 @@ function build(instance::String; dir=joinpath(dirname(@__DIR__), "instances"))
         for j in 1:n
             xⱼ = N[j].x
             yⱼ = N[j].y 
-            l  = ((xⱼ-xᵢ)^2 + (yⱼ-yᵢ)^2)^0.5
-            A[i,j] = Arc(i, j, l)
+            c = ((xⱼ-xᵢ)^2 + (yⱼ-yᵢ)^2)^0.5
+            A[i,j] = Arc(i, j, c)
         end
     end
     # vehicles
-    # TODO: This part is incorrect and inefficient
-    V = Vector{Vehicle}()
-    for i in 1:parse(Int, df[k₂, 2])
-        q = parse(Float64, df[k₂, 2])
-        push!(V, Vehicle(i, 1, 1, q, 0, 0.0))
-    end
+    d = 0
+    for i ∈ 1:n d += N[i].q end
+    m = d ÷ q + 1
+    V = Vector{Vehicle}(undef, m)
+    for i in 1:m V[i] = Vehicle(i, q) end
     # create graph
     G = (N, A, V)
     return G
