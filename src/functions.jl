8e4805474e5209071274d690db72ad517706c91e
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-09-23 10:58:13 +0530

    update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -1,13 +1,13 @@
 
-isdepot(n::Node) = isone(n.i)
+@inline isdepot(n::Node) = isone(n.i)
 
-iscustomer(n::Node) = !isone(n.i) 
+@inline iscustomer(n::Node) = !isone(n.i) 
 
-isopen(n::Node) = iszero(n.v)
+@inline isopen(n::Node) = iszero(n.v)
 
-isclose(n::Node) = !iszero(n.v)
+@inline isclose(n::Node) = !iszero(n.v)
 
-isopt(v::Vehicle) = !iszero(v.n)
+@inline isopt(v::Vehicle) = !iszero(v.n)
 
 function vectorize(s::Solution)
     Z = Int[]
@@ -17,5 +17,10 @@ function vectorize(s::Solution)
     return Z
 end
 
-f(s::Solution) = s.c + s.p * 10 ^ ceil(log10(s.c))
-h(s::Solution) = hash(s) # TODO: Alternatively, check hashing with Node Vector instead
+@inline f(s::Solution) = s.c + s.p * 10 ^ ceil(log10(s.c))
+
+@inline h(s::Solution) = hash(s) # TODO: Alternatively, check hashing with Node Vector instead
+
+@inline Base.deepcopy_internal(G::Graph, dict::IdDict) = Graph(Base.deepcopy_internal(G.N, dict), G.A, Base.deepcopy_internal(G.V, dict))
+
+@inline Base.deepcopy_internal(s::Solution, dict::IdDict) = Solution(Base.deepcopy_internal(s.G, dict), s.c, s.p)
