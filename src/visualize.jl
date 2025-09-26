43d7fd80de2709b63c412413d08b5be551e00b7d
HarshBerg <harshuket@gmail.com>
2025-09-26 14:37:40 +0530

    finished visualize solution

--- src/visualize.jl
+++ src/visualize.jl
@@ -21,15 +21,30 @@ end
 
 function visualize(s::Solution; backend=gr)
     backend()
-    # TODO
-    # figure
+    fig = plot(legend=:none)
+    G = s.G
+    N = G.N
+    V = G.V
+    K = length(N)
+    X = zeros(Float64, K)       # abcissa
+    Y = zeros(Float64, K)       # ordinate
+    C = fill("color", K)        # color
+    S = zeros(Int, K)           # size
+    M = fill(:shape, K)         # marker
+
+    for (k,n) ∈ enumerate(N)
+        X[k] = n.x
+        Y[k] = n.y
+        C[k] = isdepot(n) ? "#b4464b" : "#d1e0ec"
+        S[k] = isdepot(n) ? 6 : 5
+        M[k] = isdepot(n) ? :rect : :circle
+    end
+
+    scatter!(X, Y, color=C, markersize=S, markershape=M, markerstrokewidth=0)
+    Z = vectorize(s)
+    K = length(Z)
+    X = [N[z].x for z in Z]
+    Y = [N[z].y for z in Z]
+    plot!(X, Y, color="#4a90e2", linewidth=1, linealpha=0.5)    
     return fig
-end
-
-
-
-
-
-
-
-        
+end 
