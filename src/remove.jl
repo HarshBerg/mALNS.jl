1c8a28b53ac593507a4ee84c366f237b96a91168
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-10-03 10:14:14 +0530

    add remove.jl

---
+++ src/remove.jl
@@ -0,0 +1,62 @@
+# Random Removal
+function randomnode!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function randomarc!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function randomsegment!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function randomvehicle!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+# Related Removal
+function relatednode!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function relatedarc!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function relatedsegment!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function relatedvehicle!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+# Worst Removal
+function worstnode!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function worstarc!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function worstsegment!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
+
+function worstvehicle!(rng::AbstractRNG, k::Int, s::Solution)
+    # TODO
+    return s
+end
