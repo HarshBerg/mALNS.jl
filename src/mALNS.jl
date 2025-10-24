49e3520f4653179dc4a0b1a938a9908144d41f6d
anmpahwa <anmpahwa@civil.iitm.ac.in>
2025-10-24 11:05:43 +0530

    update mALNS

--- src/mALNS.jl
+++ src/mALNS.jl
@@ -3,7 +3,9 @@ module mALNS
 # TODO: Import packages here
 using CSV
 using Plots
+using Random
 using Revise
+using StatsBase
 using DataFrames
 
 # TODO: Include files here
@@ -11,6 +13,7 @@ include("datastructure.jl")
 include("functions.jl")
 include("operations.jl")
 include("initialize.jl")
+include("remove.jl")
 include("visualize.jl")
 
 # TODO: Export function here
