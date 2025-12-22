3570dc195c23a28055fae449985534e86c7647c9
HarshBerg <harshuket@gmail.com>
2025-12-22 10:33:44 +0530

    updated distributions

--- src/mALNS.jl
+++ src/mALNS.jl
@@ -7,6 +7,7 @@ using Random
 using Revise
 using StatsBase
 using DataFrames
+using Distributions
 
 # TODO: Include files here
 include("datastructure.jl")
@@ -15,6 +16,7 @@ include("operations.jl")
 include("initialize.jl")
 include("remove.jl")
 include("visualize.jl")
+include("insert..jl")
 
 # TODO: Export function here
 export build, initialize, f, visualize
