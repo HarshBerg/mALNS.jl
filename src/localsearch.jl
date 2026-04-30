6ac3a2b5658126e80926679f467f2cedd92198c3
anmpahwa <anmpahwa@civil.iitm.ac.in>
2026-04-30 10:43:14 +0530

    update localsearch.jl

--- src/localsearch.jl
+++ src/localsearch.jl
@@ -46,89 +46,25 @@ function move!(rng::AbstractRNG, k::Int, s::Solution; scope::Symbol)
     end
     return s
 end
+"""
+    intramove!(rng::AbstractRNG, k::Int, s::Solution)
 
+Returns solution `s` after moving a randomly selected node to its best position 
+in the same route, if the move results in a reduction in objective function value, 
+repeating for `k` iterations.    
+"""
 intramove!(rng::AbstractRNG, k::Int, s::Solution) = move!(rng, k, s; scope=:intra)
-intermove!(rng::AbstractRNG, k::Int, s::Solution) = move!(rng, k, s; scope=:inter)
-#=
-function swap!(rng::AbstractRNG, k::Int, s::Solution; scope::Symbol)
-    G = s.G
-    N = G.N
-    V = G.V
-    I = eachindex(N)
-    Wₙ = [isdepot(n) || isopen(n) ? 0 : 1 for n ∈ N]
-    Wᵥ = [[isequal(n.v, v.i) ? (isequal(scope, :intra) && v.n ≥ 2) : (isequal(scope, :inter) && isopt(v)) for v ∈ V] for n ∈ N]
-    Wₘ = [[]] 
-    if iszero(sum(Wₙ)) return s end
-    for _ ∈ 1:k
-        i = sample(rng, I, Weights(Wₙ))
-        n = N[i]
-        if iszero(sum(Wᵥ[n.i])) continue end
-        tₙ = N[n.t]
-        hₙ = N[n.h]
-        vₙ = V[n.v]
-        c = 0.
-        p = (tₙ.i, hₙ.i, vₙ.i)
-        z = f(s)
-        vₘ = sample(rng, V, Weights(Wᵥ[n.i]))
-        m  = N[vₘ.s]
-        tₘ = N[1]
-        hₘ = N[m.h]
-        for _ ∈ 0:vₘ.n
-            if isequal(n, m) continue 
-            # t-n-m-h
-            elseif isequal(n, m.t)
-                removenode!(n, tₙ, hₙ, vₙ, s)
-                insertnode!(n, m, hₘ, vₘ, s)
-                z′ = f(s)
-                Δ  = z′ - z
-                if Δ < c c, p = Δ, (tₘ.i, hₘ.i, vₘ.i) end
-                removenode!(n, m, hₘ, vₘ, s)
-                insertnode!(n, tₙ, hₙ, vₙ, s)
-            # t-n₂-n-h
-            elseif isequal(m, n.t)
-                removenode!(n, tₙ, hₙ, vₙ, s)
-                insertnode!(n, tₘ, m, vₘ, s)
-                z′ = f(s)
-                Δ  = z′ - z
-                if Δ < c c, p = Δ, (tₘ.i, hₘ.i, vₘ.i) end
-                removenode!(n, tₘ, m, vₘ, s)
-                insertnode!(n, tₙ, hₙ, vₙ, s)
-            else 
-                removenode!(n, tₙ, hₙ, vₙ, s)
-                removenode!(m, tₘ, hₘ, vₘ, s)
-                insertnode!(n, tₘ, hₘ, vₘ, s)
-                insertnode!(m, tₙ, hₙ, vₙ, s)
-                z′ = f(s)
-                Δ  = z′ - z
-                if Δ < c c, p = Δ, (tₘ.i, hₘ.i, vₘ.i) end
-                removenode!(n, tₘ, hₘ, vₘ, s)
-                removenode!(m, tₙ, hₙ, vₙ, s)
-                insertnode!(n, tₙ, hₙ, vₙ, s)
-                insertnode!(m, tₘ, hₘ, vₘ, s)
-            end
-            tₘ = m
-            m = hₘ
-            hₘ = N[m.h]
-        end
-        # re-insert at best position
-        tₘ = N[p[1]]
-        hₘ = N[p[2]]
-        vₘ = V[p[3]]
-        m  = N[hₘ.t]
-        if isequal(n, m.t)
-            insertnode!(n, m, hₘ, vₘ, s)
-        elseif isequal(m, n.t)
-            insertnode!(n, tₘ, m, vₘ, s)
-        else 
-            removenode!(m, tₘ, hₘ, vₘ, s)
-            insertnode!(n, tₘ, hₘ, vₘ, s)
-            insertnode!(m, tₙ, hₙ, vₙ, s)
-        end
-    end
-    return s
-end
-=#
+"""
+    intermove!(rng::AbstractRNG, k::Int, s::Solution)
 
+Returns solution `s` after moving a randomly selected node to its best position 
+in a randomly selected route, if the move results in a reduction in objective 
+function value, repeating for `k` iterations.    
+"""
+intermove!(rng::AbstractRNG, k::Int, s::Solution) = move!(rng, k, s; scope=:inter)
+
+
+# TODO: Added swap from my repository; checnk and verify what is the difference and remove your version.
 function swap!(rng::AbstractRNG, k::Int, s::Solution; scope::Symbol)
     G = s.G
     N = G.N
@@ -180,7 +116,308 @@ function swap!(rng::AbstractRNG, k::Int, s::Solution; scope::Symbol)
     end
     return s
 end
-
 interswap!(rng::AbstractRNG, k::Int, s::Solution) = swap!(rng, k, s; scope=:inter)
 intraswap!(rng::AbstractRNG, k::Int, s::Solution) = swap!(rng, k, s; scope=:intra)
 
+
+"""
+    swap!(rng::AbstractRNG, k::Int, s::Solution)
+
+Returns solution `s` after swapping two randomly selected nodes in the specified 
+solution `scope`, if the swap results in a reduction in objective function 
+value, repeating for `k` iterations.
+"""
+function swap!(rng::AbstractRNG, k::Int, s::Solution; scope::Symbol)
+    # tₘ → m → hₘ and tₙ → n → hₙ
+    # pre-initialize
+    G = s.G
+    N = G.N
+    V = G.V
+    # initialize
+    Wₙ = [isdepot(n) ? 0 : 1 for n ∈ N]
+    Wₘ = [[isequal(n.v, m.v) ? isequal(scope, :intra) : isequal(scope, :inter) for m ∈ N] for n ∈ N]
+    # iterate
+    for _ ∈ 1:k
+        # compute cost of the current solution
+        z = f(s)
+        # sample nodes
+        n = sample(rng, N, Weights(Wₙ))
+        m = sample(rng, N, Weights(Wₘ[n.i] .* Wₙ))
+        # swap the two nodes
+        tₙ = N[n.t]
+        hₙ = N[n.h]
+        vₙ = V[n.v]
+        tₘ = N[m.t]
+        hₘ = N[m.h]
+        vₘ = V[m.v]
+        # case 1: tₘ → m (tₙ) → n (hₘ) → hₙ
+        if isequal(n, hₘ) # || isequal(m, tₙ)
+            removenode!(n, tₙ, hₙ, vₙ, s)
+            insertnode!(n, tₘ, tₙ, vₘ, s)
+        # case 2: tₙ → n (tₘ) → m (hₙ) → hₘ
+        elseif isequal(n, tₘ) # || isequal(m, hₙ)
+            removenode!(n, tₙ, hₙ, vₙ, s)
+            insertnode!(n, hₙ, hₘ, vₘ, s)
+        # case 3: all other cases
+        else
+            removenode!(n, tₙ, hₙ, vₙ, s)
+            removenode!(m, tₘ, hₘ, vₘ, s)
+            insertnode!(n, tₘ, hₘ, vₘ, s)
+            insertnode!(m, tₙ, hₙ, vₙ, s)
+        end
+        # evaluate the change in objective function value
+        z′ = f(s)
+        c  = z′ - z
+        if c < 0 continue end
+        # reswap the two nodes
+        tₙ = N[n.t]
+        hₙ = N[n.h]
+        vₙ = V[n.v]
+        tₘ = N[m.t]
+        hₘ = N[m.h]
+        vₘ = V[m.v]
+        # case 1: tₘ → m (tₙ) → n (hₘ) → hₙ
+        if isequal(n, hₘ) # || isequal(m, tₙ)
+            removenode!(n, tₙ, hₙ, vₙ, s)
+            insertnode!(n, tₘ, tₙ, vₘ, s)
+        # case 2: tₙ → n (tₘ) → m (hₙ) → hₘ
+        elseif isequal(n, tₘ) # || isequal(m, hₙ)
+            removenode!(n, tₙ, hₙ, vₙ, s)
+            insertnode!(n, hₙ, hₘ, vₘ, s)
+        # case 3: all other cases
+        else
+            removenode!(n, tₙ, hₙ, vₙ, s)
+            removenode!(m, tₘ, hₘ, vₘ, s)
+            insertnode!(n, tₘ, hₘ, vₘ, s)
+            insertnode!(m, tₙ, hₙ, vₙ, s)
+        end
+    end
+    # return solution
+    return s
+end
+"""
+    intraswap!(rng::AbstractRNG, k::Int, s::Solution)
+
+Returns solution `s` after swapping two randomly selected nodes from 
+the same route, if the swap results in a reduction in objective function 
+value, repeating for `k` iterations.
+"""
+intraswap!(rng::AbstractRNG, k::Int, s::Solution) = swap!(rng, k, s; scope=:intra)
+"""
+    interswap!(rng::AbstractRNG, k::Int, s::Solution)
+
+Returns solution `s` after swapping two randomly selected nodes from 
+two different routes, if the swap results in a reduction in objective 
+function value, repeating for `k` iterations.
+"""
+interswap!(rng::AbstractRNG, k::Int, s::Solution) = swap!(rng, k, s; scope=:inter)
+
+"""
+    intraopt!(rng::AbstractRNG, k::Int, s::Solution)
+
+Returns solution `s` after iteratively taking 2 arcs from the same route 
+and reconfiguring them (total possible reconfigurations 2₂-1 = 3) if the 
+reconfiguration results in a reduction in objective function value, repeating 
+for `k` iterations.
+"""
+function intraopt!(rng::AbstractRNG, k::Int, s::Solution)
+    # pre-initialize
+    G = s.G
+    N = G.N
+    V = G.V
+    # initialize
+    Wₙ = [isdepot(n) ? 0 : 1 for n ∈ N]
+    Wₘ = [[isequal(n.v, m.v) ? (isequal(n, m) ? 0 : 1) : 0 for m ∈ N] for n ∈ N]
+    # iterate
+    for _ ∈ 1:k
+        # compute cost of the current solution
+        z = f(s)
+        # sample nodes
+        n = sample(rng, N, Weights(Wₙ))
+        m = sample(rng, N, Weights(Wₘ[n.i]))
+        # order correction
+        p = N[n.t]
+        while !isdepot(p)
+            if isequal(p, m)
+                m = n
+                n = p
+                break
+            end
+            p = N[p.t]
+        end
+        # skip invalid configurations
+        if isdepot(n) continue end
+        if isdepot(m) continue end
+        if isequal(n, m) continue end
+        if !isequal(n.v, m.v) continue end
+        # perform operations
+        # fetch vehicle
+        vₒ = V[n.v]
+        # set pivot node
+        p  = N[m.i]
+        # fetch tail and head node of new position
+        tₙ = N[n.i]
+        hₙ = N[n.h]
+        # fetch tail and head node of original position
+        tₒ = N[p.t]
+        hₒ = N[p.h]
+        # move nodes
+        b = N[n.h]
+        while !isequal(p.i, b.i)
+            removenode!(p, tₒ, hₒ, vₒ, s)
+            insertnode!(p, tₙ, hₙ, vₒ, s)
+            tₙ = N[p.i]
+            hₙ = N[p.h]
+            p  = N[tₒ.i]
+            tₒ = N[p.t]
+            hₒ = N[p.h]
+        end
+        m = p
+        # evaluate the change in objective function value
+        z′ = f(s)
+        c  = z′ - z
+        if c < 0 continue end
+        # reperform operations
+        # set pivot node
+        p  = N[m.i]
+        # fetch tail and head node of new position
+        tₙ = N[n.i]
+        hₙ = N[n.h]
+        # fetch tail and head node of original position
+        tₒ = N[p.t]
+        hₒ = N[p.h]
+        # move nodes
+        b = N[n.h]
+        while !isequal(p.i, b.i)
+            removenode!(p, tₒ, hₒ, vₒ, s)
+            insertnode!(p, tₙ, hₙ, vₒ, s)
+            tₙ = N[p.i]
+            hₙ = N[p.h]
+            p  = N[tₒ.i]
+            tₒ = N[p.t]
+            hₒ = N[p.h]
+        end
+        m = p
+    end
+    # return solution
+    return s
+end
+
+"""
+    interopt!(rng::AbstractRNG, k::Int, s::Solution)
+
+Returns solution `s` after iteratively taking 2 arcs from different routes 
+and reconfiguring them (total possible reconfigurations 2₂-1 = 3) if the 
+reconfiguration results in a reduction in objective function value, repeating 
+for `k` iterations.
+"""
+function interopt!(rng::AbstractRNG, k::Int, s::Solution)
+    # pre-initialize
+    G = s.G
+    N = G.N
+    V = G.V
+    # initialize
+    Wₙ = [isdepot(n) ? 0 : 1 for n ∈ N]
+    Wₘ = [[isequal(n.v, m.v) ? 0 : 1 for m ∈ N] for n ∈ N]
+    # iterate
+    for _ ∈ 1:k
+        # compute cost of the current solution
+        z = f(s)
+        # sample nodes
+        n = sample(rng, N, Weights(Wₙ))
+        m = sample(rng, N, Weights(Wₘ[n.i]))
+        # skip invalid configurations
+        if isdepot(n) continue end
+        if isdepot(m) continue end
+        if isequal(n.v, m.v) continue end
+        # perform operations
+        # fetch vehicles
+        vₙ = V[n.v]
+        vₘ = V[m.v]
+        # fetch tail and head nodes in vehicle n
+        tₙ = N[n.i]
+        hₙ = N[n.h]
+        # set pivot node
+        p  = N[m.i]
+        # fetch tail and head nodes in vehicle m
+        tₘ = N[p.t]
+        hₘ = N[p.h]
+        # move nodes from vehicle m to n
+        while !isdepot(p)
+            removenode!(p, tₘ, hₘ, vₘ, s)
+            insertnode!(p, tₙ, hₙ, vₙ, s)
+            # update nodes
+            tₙ = p
+            hₙ = N[tₙ.h]
+            p  = hₘ
+            tₘ = N[p.t]
+            hₘ = N[p.h]
+        end
+        m = hₙ
+        # fetch tail and head nodes in vehicle m
+        tₘ = N[vₘ.e]
+        hₘ = N[1]
+        # set pivot node
+        p  = hₙ
+        # fetch tail and head nodes in vehicle n
+        tₙ = N[p.t]
+        hₙ = N[p.h]
+        # move nodes from vehicle n to m
+        while !isdepot(p)
+            removenode!(p, tₙ, hₙ, vₙ, s)
+            insertnode!(p, tₘ, hₘ, vₘ, s)
+            # update nodes
+            tₘ = p
+            hₘ = N[tₘ.h]
+            p  = hₙ
+            tₙ = N[p.t]
+            hₙ = N[p.h]
+        end
+        # evaluate the change in objective function value
+        z′ = f(s)
+        c  = z′ - z
+        if c < 0 continue end
+        # reperform operations
+        # fetch tail and head nodes in vehicle n
+        tₙ = N[n.i]
+        hₙ = N[n.h]
+        # set pivot node
+        p  = N[m.i]
+        # fetch tail and head nodes in vehicle m
+        tₘ = N[p.t]
+        hₘ = N[p.h]
+        # move nodes from vehicle m to n
+        while !isdepot(p)
+            removenode!(p, tₘ, hₘ, vₘ, s)
+            insertnode!(p, tₙ, hₙ, vₙ, s)
+            # update nodes
+            tₙ = p
+            hₙ = N[tₙ.h]
+            p  = hₘ
+            tₘ = N[p.t]
+            hₘ = N[p.h]
+        end
+        m = hₙ
+        # fetch tail and head nodes in vehicle m
+        tₘ = N[vₘ.e]
+        hₘ = N[1]
+        # set pivot node
+        p  = hₙ
+        # fetch tail and head nodes in vehicle n
+        tₙ = N[p.t]
+        hₙ = N[p.h]
+        # move nodes from vehicle n to m
+        while !isdepot(p)
+            removenode!(p, tₙ, hₙ, vₙ, s)
+            insertnode!(p, tₘ, hₘ, vₘ, s)
+            # update nodes
+            tₘ = p
+            hₘ = N[tₘ.h]
+            p  = hₙ
+            tₙ = N[p.t]
+            hₙ = N[p.h]
+        end
+    end
+    # return solution
+    return s
+end
