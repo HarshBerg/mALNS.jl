663a9b7ff4ffaed3000d86d542545119551c4de9
anmpahwa <anmpahwa@civil.iitm.ac.in>
2026-05-03 16:49:07 +0530

    update mALNS

--- src/mALNS.jl
+++ src/mALNS.jl
@@ -1,6 +1,5 @@
 module mALNS
 
-# TODO: Import packages here
 using CSV
 using Plots
 using Random
@@ -10,22 +9,32 @@ using DataFrames
 using ProgressMeter
 using Distributions
 
-# TODO: Include files here
 include("datastructure.jl")
 include("functions.jl")
-include("operations.jl")
 include("initialize.jl")
+include("operations.jl")
 include("remove.jl")
-include("visualize.jl")
 include("insert.jl")
 include("localsearch.jl")
 include("parameters.jl")
 include("ALNS.jl")
+include("visualize.jl")
 
-# TODO: Export function here
-export build, initialize, f, visualize, ALNSparameters, benchmark, isfeasible, modALNS, conALNS,  
-    randomnode!, randomarc!, randomsegment!, relatednode!, relatedarc!, relatedsegment!, worstnode!, worstarc!, worstsegment!,
-    bestprecise!, bestperturb!, greedyprecise!, greedyperturb!, regret2precise!, regret2perturb!, regret3precise!, regret3perturb!,
-    intermove!, intramove!, interswap!, intraswap!
+export  Node, Arc, Vehicle, Graph, Solution,
+        build, initialize,
+        randomnode!, randomarc!, randomsegment!, randomvehicle!,
+        relatednode!, relatedarc!, relatedsegment!, relatedvehicle!,
+        worstnode!, worstarc!, worstsegment!, worstvehicle!,
+        bestprecise!, bestperturb!,
+        greedyprecise!, greedyperturb!,
+        regret2precise!, regret2perturb!,
+        regret3precise!, regret3perturb!,
+        regret5precise!, regret5perturb!,
+        intramove!, intermove!,
+        intraswap!, interswap!,
+        intraopt!, interopt!,
+        vectorize, sol, isfeasible, f, h,
+        ALNSparameters, ALNS, 
+        visualize, pltcnv
 
 end
